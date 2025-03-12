33108257250d6ed8c925931eefc893fff14044f7
Alan Rodriguez <alanrodriguez102347@gmail.com>
2025-03-11 20:15:50 -0700

    Initial Commit

---
+++ Wide CameraTests/Wide_CameraTests.swift
@@ -0,0 +1,17 @@
+//
+//  Wide_CameraTests.swift
+//  Wide CameraTests
+//
+//  Created by Alan Rodriguez on 3/11/25.
+//
+
+import Testing
+@testable import Wide_Camera
+
+struct Wide_CameraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
